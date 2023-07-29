a9c43372881261a2659d67c01b3ba1932083aee0
Bohdan <bohdan.dudchenko@quintagroup.org>
2023-07-29 14:23:21 +0300

    Initial version

---
+++ libraries/testrail.py
@@ -0,0 +1,19 @@
+import decorator
+
+
+class testrail:
+    """
+    Decorator to decorate tests for easier identification and printing out their IDs
+    """
+    def __init__(self, test_case_id, output=True):
+        self.test_case_id = test_case_id
+        self.output = output
+
+    def __call__(self, fn):
+        def wrapper(fn, fn_self, *args):
+            if self.output:
+                print(self.test_case_id, end=" ")
+
+            return fn(fn_self, *args)
+
+        return decorator.decorator(wrapper, fn)
